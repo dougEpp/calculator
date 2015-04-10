aca178ebebf48271e6276065b1fae968de12440a
Andrew Hartwig <andrew.hartwig1@gmail.com>
2015-04-09 22:13:46 -0400

    Economic value by Andrew

--- js/controllers/formsController.js
+++ js/controllers/formsController.js
@@ -43,18 +43,37 @@ App.controller('originalLoanAndBalanceCtrl', function($scope, $window) {
 	$scope.IP = function() { return ($scope.PMT * $scope.N) - $scope.PV();};
 	});
     
-App.controller('compareEconomicValueCtrl', function($scope, $window) {
+App.controller('compareEconomicValueLump', function($scope, $window) {
+    $scope.FV = 100000;
+	$scope.I = 5;
+	$scope.N = 5;
+    $scope.downPayment = 25000;
 
-	$scope.PMT = 450;
-	$scope.I = 8;
-	$scope.Icm = function() { return ($scope.I / 100) / 12;};
-	$scope.N = 9;
-	$scope.Tempiplus1 = function() { return (1 + parseFloat($scope.Icm()));};
-	
-	$scope.Iplus1s = function() { return $window.Math.pow($scope.Tempiplus1(), -$scope.N);};
-	$scope.PV = function(){ return (parseFloat($scope.PMT) * (1 - ($scope.Iplus1s()))/parseFloat($scope.Icm()));};
-	$scope.IP = function() { return ($scope.PMT * $scope.N) - $scope.PV();};
-	});
+    $scope.PV = function() {
+        return (parseFloat($scope.FV)) / ($window.Math.pow(1 + (parseFloat($scope.I) / 100), $scope.N));
+    }
+    $scope.total = function() {
+        return $scope.PV() + parseFloat($scope.downPayment);
+    }
+});
+App.controller('compareEconomicValuePmt', function($scope, $window) {
+    $scope.PMT = 20000;
+    $scope.downPayment = 16000;
+    $scope.I = 5;
+    $scope.N = 5;
+
+    $scope.PV = function() {
+        var presentValue = $scope.PMT;
+        for (var i = 0; i < $scope.N - 1; i++) {
+            presentValue = ((presentValue) / ($window.Math.pow(1 + (parseFloat($scope.I) / 100), 1))) + $scope.PMT;
+        }
+        return ((presentValue) / ($window.Math.pow(1 + (parseFloat($scope.I) / 100), 1)));
+    }
+
+    $scope.total = function() {
+        return $scope.PV() + $scope.downPayment;
+    }
+});
     
     
     
